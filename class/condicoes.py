19d993657ce5b17df6f5f5ba4a170f3056c059ac
Torresamanda <amandakaia@hotmail.com>
2023-01-04 11:06:56 -0300

    aula de condições (if e else)

---
+++ class/condicoes.py
@@ -0,0 +1,19 @@
+# nome = str(input('Qual o seu nome? '))
+# if nome == 'Gustavo':
+#     print('Que nome lindo voCê tem!')
+# else:
+#     print('Seu nome é tão normal')
+# print('Bom dia, {}!'.format(nome))
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1 + n2) / 2
+
+print('A sua média foi {:.1f}'.format(m))
+print('Parabéns' if m >= 6 else 'Estude Mais!')
+
+# if m>= 6.0:
+#     print('Sua média foi boa!')
+# else:
+#     print('Sua média foi ruim, estude mais!') 
+    
