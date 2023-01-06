5c7b79f6f14e449eeb819688e3787568f5874c99
Torresamanda <amandakaia@hotmail.com>
2023-01-06 10:53:28 -0300

    aula de estrutura de repetição (for)

---
+++ mundo02-estrutura-de-controle/class/estrutura-de-repeticao.py
@@ -0,0 +1,23 @@
+# for c in range(1, 7, 2):
+#     print(c)
+# print('Fim')
+
+# n = int(input('Digite um número: '))
+
+# for c in range(0, n+1):
+#     print(c)
+# print('Fim')
+
+# i = int(input('Início: '))
+# f = int(input('Fim: '))
+# p = int(input('Passo: '))
+
+# for c in range(i, f+1, p):
+#     print(c)
+# print('Fim')
+
+s = 0
+for c in range(0, 3):
+    n = int(input('Digite um valor: '))
+    s += n 
+print('A soma dos valores foi {}'.format(s))
