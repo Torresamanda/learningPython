bf947354be543516071e67df95a6ed7dae565f3c
Torresamanda <amandakaia@hotmail.com>
2023-01-11 11:23:37 -0300

    aula de tuplas

---
+++ mundo03/class/tuplas.py
@@ -0,0 +1,27 @@
+lanches = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+
+# print(lanches[1:])
+
+# for cont in range(0, len(lanches)):
+#     print(f'Eu vou comer {lanches[cont]}')
+
+# for comida in lanches:
+#     print(f'Eu vou comer {comida}!')
+
+# for pos, comida in enumerate(lanches):
+#     print(f'Eu vou comer {comida} na posição {pos}')
+
+# print('Eu comi demais!')
+
+## Ordenar listas
+# print(sorted(lanches))
+
+# a = (2, 5, 4)
+# b = (5, 8, 9, 10)
+# c = a + b
+# print(c.index(5, 1))
+
+pessoa = ('Gustavo', 39, 'M', 99.88)
+del(pessoa)
+print(pessoa)
+
